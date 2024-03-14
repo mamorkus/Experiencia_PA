46979c1299d62059f03903553d3084d30e0cc69a
karlaaalcubo <163366635+karlaaalcubo@users.noreply.github.com>
2024-03-14 13:40:15 +0000

    juego terminado

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,4 +1,20 @@
+from random import randint
 def memoria():
+    N=int(input('Elige el nivel del desafío: '))
+    print('Memoriza la siguiente secuencia: ')
+    sec=''
+    for i in range(N):
+        num=randint(0,9)
+        sec+=str(num)+' '
+    sec=sec[:-1]
+    print(sec)
+    print('Ahora repite la secuencia')
+    resp=input()
+    if resp==sec:
+        print('Felicidades! Has acertado')
+    else:
+        print('Incorrecto :c')
+
     """
     Esta función representa el juego de memoria.
     Debes generar una secuencia de números al azar y mostrarla al usuario.
@@ -6,3 +22,4 @@ def memoria():
     Se debe mostrar un mensaje si el usuario acierta o no.
     """
     pass
+memoria()
