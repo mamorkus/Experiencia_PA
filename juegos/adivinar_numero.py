e15620053731cb01eb90390cd0dbbf6dc355ecf0
wab <163414468+mamorkus@users.noreply.github.com>
2024-03-14 13:35:01 +0000

    git add

--- juegos/adivinar_numero.py
+++ juegos/adivinar_numero.py
@@ -1,7 +1,15 @@
+import random as ran
 def adivinar_numero():
     """
     Esta función representa el juego de adivinar un número.
     Debes generar un número al azar entre 1 y 10, y luego pedir al usuario que adivine el número.
     Se debe mostrar un mensaje si el usuario adivina correctamente o no.
     """
-    pass
+    a = ran.randint(1,10)
+    num = int(input("Escoge un numero del 1 al 10: "))
+    if num == a:
+        print("Adivinaste Correctamente!!")
+    else:
+        print("Perdiste :c")
+        print("El número correcto era",a)
+adivinar_numero()
--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,7 +1,17 @@
+import random as r
 def cachipun():
     """
     Esta función representa el juego de cachipun.
     Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
     La computadora debe elegir una opción al azar.
     """
-    pass
+    com = r.randint(0,2)
+    eleccion = ["piedra","papel","tijera"]
+    player = int(input("Elige una opción (0 = Piedra, 1 = Papel, 2 = Tijera): "))
+    if com == player:
+        print("Empate! Los dos escogieron",eleccion[com])
+    elif (com == 0 and player == 1) or (com == 1 and player == 2) or (com ==2 and player == 1):
+        print("Has Ganado!! El computador eligió",eleccion[com])
+    else:
+        print("Perdiste :c El computador eligió",eleccion[com])
+cachipun()
